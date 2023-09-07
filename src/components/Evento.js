3dbf0ae0be72c22d615e6acc4bb383460a077982
Jeron Windson <jeron_windson@hotmail.com>
2023-09-07 14:58:51 -0300

    updade evento com Props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,11 +1,18 @@
+import Button from './evento/Button';
+
 function Evento() {
     function meuEvento() {
-        console.log('Opa, fui ativado!');
+        console.log('Ativando primeiro Evento!');
+    }
+
+    function segundoEvento() {
+        console.log("Ativando segundo evento!");
     }
     return(
         <>
         <p>clique para disparar um evento:</p>
-        <button onClick={meuEvento}>Ativar!</button>
+        <Button event={meuEvento} text="Primeiro Evento" />
+        <Button event={segundoEvento} text="Segundo Evento" />
         </>
     )
 }
