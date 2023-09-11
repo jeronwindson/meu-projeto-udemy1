c59e2bee1bb1ed82e377fdd3a615136e758f8224
Jeron Windson <jeron_windson@hotmail.com>
2023-09-11 13:54:12 -0300

    updade Rotas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const [nome, setNome] = useState()
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome} />
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>}>
+          </Route>
+          <Route path="/empresa" element={<Empresa/>}>
+          </Route>
+          <Route path="/contato" element={<Contato/>}>
+          </Route>
+        </Routes>
+        <Footer />
+      </Router>
   )
-}
-
+};
 
 export default App;
